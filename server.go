76c13a132c085c1b8c282eb772791e831cf09713
Long Nguyen <lnguyen@engineyard.com>
2013-10-24 11:31:51 -0700

    trying go.grace

--- server.go
+++ server.go
@@ -2,11 +2,9 @@ package main
 
 import (
   "github.com/gorilla/mux"
-  "github.com/longnguyen11288/goagain"
+  "github.com/gorilla/handlers"
+  "github.com/daaku/go.grace/gracehttp"
   "net/http"
-  "net"
-  "time"
-  "log"
   "os"
 )
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,59 +16,15 @@ func main() {
   r := mux.NewRouter()
   r.HandleFunc("/", HomeHandler)
 
-  http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
+
+  r.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w, r, r.URL.Path[1:])
   })
-  http.Handle("/", r)
+  loggingHandler := handlers.LoggingHandler(os.Stdout, r)
+  http.Handle("/", loggingHandler)
 
-  var (
-    err error
-    l net.Listener
-    ppid int
+  gracehttp.Serve(
+    &http.Server{Addr: ":8080", Handler: loggingHandler },
   )
-
-  l, ppid, err = goagain.GetEnvs()
-
-  if err != nil {
-    // Listen on a TCP or a UNIX domain socket (the latter is commented).
-    laddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:48879")
-    if nil != err {
-      log.Fatalln(err)
-    }
-    log.Printf("listening on %v", laddr)
-    l, err = net.ListenTCP("tcp", laddr)
-    if nil != err {
-      log.Fatalln(err)
-    }
-
-    // Accept connections in a new goroutine.
-    go http.Serve(l,nil);
-  } else {
-    // Resume listening and accepting connections in a new goroutine.
-    log.Printf("resuming listening on %v", l.Addr())
-    go http.Serve(l,nil);
-
-    // Kill the parent, now that the child has started successfully.
-    if err := goagain.KillParent(ppid); nil != err {
-      log.Fatalln(err)
-    }
-  }
-
-  // Block the main goroutine awaiting signals.
-  if err := goagain.AwaitSignals(l); nil != err {
-    log.Fatalln(err)
-  }
-  log.Println(os.Args[0])
-
-  // Do whatever's necessary to ensure a graceful exit like waiting for
-  // goroutines to terminate or a channel to become closed.
-
-  // In this case, we'll simply stop listening and wait one second.
-  if err := l.Close(); nil != err {
-    log.Fatalln(err)
-  }
-  time.Sleep(1e9)
-
-
 
 }
